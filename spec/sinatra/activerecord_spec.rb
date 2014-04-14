3df57e92c6da62096fed0ef97ae5069407fdbb74
Maarten Claes <maartencls@gmail.com>
2014-04-14 21:32:21 +0200

    Doesn't work anymore with ActiveRecord 4.1.0
    From what I could tell from the rails source it seems like this is no
    longer supported.
    
    https://github.com/rails/rails/blob/v4.1.0/activerecord/lib/active_record/connection_adapters/connection_specification.rb#L123-L158

--- spec/sinatra/activerecord_spec.rb
+++ spec/sinatra/activerecord_spec.rb
@@ -98,16 +98,6 @@ arbitrary_environment:
       @app.set :environment, :development
       expect { @app.set :database_file, "database.yml" }.to raise_error(ActiveRecord::AdapterNotSpecified)
     end
-
-    it "handles non-namespaced database.yml" do
-      File.open("tmp/database.yml", "w") do |file|
-        file.write <<-EOS
-adapter: "sqlite3"
-database: "tmp/foo.sqlite3"
-        EOS
-      end
-      expect { @app.set :database_file, "database.yml" }.to establish_database_connection
-    end
   end
 
   context "DATABASE_URL is set" do
